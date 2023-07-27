a6ce848e1fea86af593f105518ac04b18cbff1be
Timothy Mwangi <133085810+RealTim0@users.noreply.github.com>
2023-07-27 12:39:21 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ const mongoose  =require ('mongoose')
 mongoose.connect(process.env.MONGO_URI)
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin:"https://riri-car-repair-shop-frontend.vercel.app"
+    }
+))
 app.post('/api/contact', sendEmail)
 app.use('/api/user', userroutes )
 app.use('/api/booking', bookingroutes)
