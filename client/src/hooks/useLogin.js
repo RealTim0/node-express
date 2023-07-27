3d39d4e7656c68517708bc38aa0d4909e8336935
Timothy Mwangi <133085810+RealTim0@users.noreply.github.com>
2023-07-27 12:18:04 +0400

    Update useLogin.js

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -12,7 +12,7 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('http://localhost:5000/api/user/login/', {
+        const response = await fetch('https://riri-car-repair-backened.vercel.app/api/user/login/', {
             method:'POST',
             headers:{'Content-Type':'application/json'},
             body:JSON.stringify({email,password})  
