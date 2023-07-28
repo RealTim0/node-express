88102bd7d35fbd00b7befa5144ec2d0e384d5024
Timothy Mwangi <133085810+RealTim0@users.noreply.github.com>
2023-07-28 15:03:24 +0400

    Update Contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -15,7 +15,7 @@ export default function Contact () {
   
   const handleSubmit = async (e) => { 
     e.preventDefault();
-    await fetch('http://localhost:5000/api/contact', {
+    await fetch('https://riri-car-repair-backend.vercel.app/api/contact', {
                 method:'POST',
                 headers:{'Content-Type':'application/json'},
                 body:JSON.stringify({name,email,subject,message})  
