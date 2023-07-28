a423480380e6f70ed61b8e40f28b070a28b602a4
Timothy Mwangi <133085810+RealTim0@users.noreply.github.com>
2023-07-28 15:01:36 +0400

    Update Booking.js

--- client/src/pages/Booking.js
+++ client/src/pages/Booking.js
@@ -19,7 +19,7 @@ const BookingForm = () => {
     const handleSubmit = async (e)=>{
       e.preventDefault()
       setIsloading(true)
-      const response = await fetch("http://localhost:5000/api/booking",{
+      const response = await fetch("https://riri-car-repair-backend.vercel.app/api/booking/",{
           method:'POST',
           body:JSON.stringify({name, email, model, phone, service, appointmentDate}),
           headers:{
